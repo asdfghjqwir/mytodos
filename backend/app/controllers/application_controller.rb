715b35343cdf4c88677a2e2f674b610e040f41b9
EC2 Default User <ec2-user@ip-172-31-45-226.eu-north-1.compute.internal>
2025-04-10 07:08:23 +0000

    今日の作業進捗

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::API
-  
-  include ActionController::MimeResponds
-  respond_to :json
-
-  def current_user
-    current_api_v1_user
-  end
 end
--- backend/db/migrate/20250405044233_add_jti_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddJtiToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :jti, :string
-  end
-end
